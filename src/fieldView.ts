5de31ae2272146b1581211d4bb2e9d5efa8d2626
1shevelov <shevelov@gmail.com>
2024-08-11 13:51:25 +0400

    Prettier disrepancies fixed

--- src/fieldView.ts
+++ src/fieldView.ts
@@ -4,92 +4,87 @@ import { GameEvents, shuffleArray } from "./config";
 import { getAllFaces } from "./assets";
 
 export class FieldView extends Container {
-  private app: Application;
-  private events: EventEmitter;
-  private rows: number;
-  private cols: number;
-  private field: CardView[] = [];
+	private app: Application;
+	private events: EventEmitter;
+	private rows: number;
+	private cols: number;
+	private field: CardView[] = [];
 
-  private readonly VERT_BORDER = [100, 100];
-  private readonly HOR_BORDER = [100, 100];
-  private readonly GAP = 0.2; // of the card size
-  private readonly rowsMax = [6, 12, 18, 24];
-  private readonly CARDS_SHOW_DELAY = 2000;
+	private readonly VERT_BORDER = [100, 100];
+	private readonly HOR_BORDER = [100, 100];
+	private readonly GAP = 0.2; // of the card size
+	private readonly rowsMax = [6, 12, 18, 24];
+	private readonly CARDS_SHOW_DELAY = 2000;
 
-  constructor(app: Application, events: EventEmitter, cards: number[]) {
-    super();
-    this.app = app;
-    this.events = events;
-    this.configureField(cards.length);
-    this.makeField(cards);
-    this.app.stage.addChild(this);
+	constructor(app: Application, events: EventEmitter, cards: number[]) {
+		super();
+		this.app = app;
+		this.events = events;
+		this.configureField(cards.length);
+		this.makeField(cards);
+		this.app.stage.addChild(this);
 
-    this.events.on(
-      GameEvents.CARDS_NOT_MATCHED,
-      this.handleCardsNotMatched,
-      this
-    );
-    this.events.on(GameEvents.CARDS_MATCHED, this.handleCardsMatched, this);
-  }
+		this.events.on(GameEvents.CARDS_NOT_MATCHED, this.handleCardsNotMatched, this);
+		this.events.on(GameEvents.CARDS_MATCHED, this.handleCardsMatched, this);
+	}
 
-  private configureField(cardCount: number): void {
-    for (let i = 0; i < this.rowsMax.length; i++) {
-      if (cardCount < this.rowsMax[i]) {
-        this.rows = i + 1;
-        this.cols = Math.ceil(cardCount / this.rows);
-        break;
-      }
-    }
-    console.log(`rows: ${this.rows}, cols: ${this.cols}`);
-  }
+	private configureField(cardCount: number): void {
+		for (let i = 0; i < this.rowsMax.length; i++) {
+			if (cardCount < this.rowsMax[i]) {
+				this.rows = i + 1;
+				this.cols = Math.ceil(cardCount / this.rows);
+				break;
+			}
+		}
+		console.log(`rows: ${this.rows}, cols: ${this.cols}`);
+	}
 
-  private makeField(cards: number[]): void {
-    let allFaces = getAllFaces();
-    allFaces = shuffleArray(allFaces);
-    const cardSizeX =
-      (this.app.screen.width - this.HOR_BORDER[0] - this.HOR_BORDER[1]) /
-      (this.cols * (this.GAP + 1) - this.GAP);
-    const cardSizeY =
-      (this.app.screen.height - this.VERT_BORDER[0] - this.VERT_BORDER[1]) /
-      (this.rows * (this.GAP + 1) - this.GAP);
-    let x: number;
-    let y: number;
-    let card: CardView;
-    let cardsIndex = 0;
-    // TODO:
-    for (let i = 0; i < this.cols; i++) {
-      for (let j = 0; j < this.rows; j++) {
-        x =
-          this.HOR_BORDER[0] + cardSizeX * 0.5 + i * cardSizeX * (1 + this.GAP);
-        y =
-          this.VERT_BORDER[0] +
-          cardSizeY * 0.5 +
-          j * cardSizeY * (1 + this.GAP);
-        card = new CardView(this.events, allFaces[cards[cardsIndex]], cardsIndex);
-        card.changeSize({ x: cardSizeX, y: cardSizeY });
-        card.changePosition({ x, y });
-        this.addChild(card);
-        this.field.push(card);
-        if (++cardsIndex >= cards.length) break;
-      }
-    }
-  }
+	private makeField(cards: number[]): void {
+		let allFaces = getAllFaces();
+		allFaces = shuffleArray(allFaces);
+		const cardSizeX =
+			(this.app.screen.width - this.HOR_BORDER[0] - this.HOR_BORDER[1]) /
+			(this.cols * (this.GAP + 1) - this.GAP);
+		const cardSizeY =
+			(this.app.screen.height - this.VERT_BORDER[0] - this.VERT_BORDER[1]) /
+			(this.rows * (this.GAP + 1) - this.GAP);
+		let x: number;
+		let y: number;
+		let card: CardView;
+		let cardsIndex = 0;
+		// TODO:
+		for (let i = 0; i < this.cols; i++) {
+			for (let j = 0; j < this.rows; j++) {
+				x = this.HOR_BORDER[0] + cardSizeX * 0.5 + i * cardSizeX * (1 + this.GAP);
+				y =
+					this.VERT_BORDER[0] +
+					cardSizeY * 0.5 +
+					j * cardSizeY * (1 + this.GAP);
+				card = new CardView(this.events, allFaces[cards[cardsIndex]], cardsIndex);
+				card.changeSize({ x: cardSizeX, y: cardSizeY });
+				card.changePosition({ x, y });
+				this.addChild(card);
+				this.field.push(card);
+				if (++cardsIndex >= cards.length) break;
+			}
+		}
+	}
 
-  private handleCardsNotMatched(openedCardsIndices: number[]): void {
-    console.log(`Cards not matched: ${openedCardsIndices.join(", ")}`);
-    this.events.emit(GameEvents.PAUSED);
-    setTimeout(() => {
-      openedCardsIndices.forEach((i) => this.field[i].hide());
-      this.events.emit(GameEvents.RESUMED);
-    }, this.CARDS_SHOW_DELAY);
-  }
+	private handleCardsNotMatched(openedCardsIndices: number[]): void {
+		console.log(`Cards not matched: ${openedCardsIndices.join(", ")}`);
+		this.events.emit(GameEvents.PAUSED);
+		setTimeout(() => {
+			openedCardsIndices.forEach((i) => this.field[i].hide());
+			this.events.emit(GameEvents.RESUMED);
+		}, this.CARDS_SHOW_DELAY);
+	}
 
-  private handleCardsMatched(openedCardsIndices: number[]): void {
-    console.log(`Cards matched: ${openedCardsIndices.join(", ")} !`);
-    this.events.emit(GameEvents.PAUSED);
-    setTimeout(() => {
-      openedCardsIndices.forEach((i) => this.field[i].remove());
-      this.events.emit(GameEvents.RESUMED);
-    }, this.CARDS_SHOW_DELAY);
-  }
+	private handleCardsMatched(openedCardsIndices: number[]): void {
+		console.log(`Cards matched: ${openedCardsIndices.join(", ")} !`);
+		this.events.emit(GameEvents.PAUSED);
+		setTimeout(() => {
+			openedCardsIndices.forEach((i) => this.field[i].remove());
+			this.events.emit(GameEvents.RESUMED);
+		}, this.CARDS_SHOW_DELAY);
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,45 @@
 import {
-  Application,
-  AbstractRenderer,
-  // Assets,
-  EventEmitter,
+	Application,
+	AbstractRenderer,
+	// Assets,
+	EventEmitter,
 } from "pixi.js";
 import { LoadAssets } from "./assets";
 import { FieldView } from "./fieldView";
 import { GameOfCards } from "./gameOfCards";
 
 (async () => {
-  const CANVAS_ID = "game-canvas";
-  const app = new Application();
-  AbstractRenderer.defaultOptions.roundPixels = true; // Crisp pixels
-  // AbstractRenderer.defaultOptions.resolution = window.devicePixelRatio || 1; // Crisp pixels
+	const CANVAS_ID = "game-canvas";
+	const app = new Application();
+	AbstractRenderer.defaultOptions.roundPixels = true; // Crisp pixels
+	// AbstractRenderer.defaultOptions.resolution = window.devicePixelRatio || 1; // Crisp pixels
 
-  const events = new EventEmitter();
+	const events = new EventEmitter();
 
-  // Initialize the application
-  await app.init({
-    // width: 800,
-    // height: 600,
-    background: "#1099bb",
-    resizeTo: window,
-    canvas: document.getElementById(CANVAS_ID) as HTMLCanvasElement,
-  });
-  // document.body.appendChild(app.canvas);
+	// Initialize the application
+	await app.init({
+		// width: 800,
+		// height: 600,
+		background: "#1099bb",
+		resizeTo: window,
+		canvas: document.getElementById(CANVAS_ID) as HTMLCanvasElement,
+	});
+	// document.body.appendChild(app.canvas);
 
-  await LoadAssets()
-  // .then(() => {
-  //   console.log("Assets loaded");
-  //   const gameOfCards = new GameOfCards(events);
-  //   new FieldView(app, events, gameOfCards.getCards());
-  // });
-  // console.log(Assets.cache);
+	await LoadAssets();
+	// .then(() => {
+	//   console.log("Assets loaded");
+	//   const gameOfCards = new GameOfCards(events);
+	//   new FieldView(app, events, gameOfCards.getCards());
+	// });
+	// console.log(Assets.cache);
 
-  const gameOfCards = new GameOfCards(events);
-  new FieldView(app, events, gameOfCards.getCards());
+	const gameOfCards = new GameOfCards(events);
+	new FieldView(app, events, gameOfCards.getCards());
 
-  app.ticker.add((time) => {
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    // bunny.rotation += 0.001 * time.deltaTime;
-  });
+	app.ticker.add((time) => {
+		// * Delta is 1 if running at 100% performance *
+		// * Creates frame-independent transformation *
+		// bunny.rotation += 0.001 * time.deltaTime;
+	});
 })();
